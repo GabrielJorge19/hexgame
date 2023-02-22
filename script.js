9e0b034402e39abe734296f50a04c87543f5ec01
Gabriel Jorge <gabrielbielcruz1.007@gmail.com>
2023-02-22 09:58:22 -0300

    first stable

--- script.js
+++ script.js
@@ -1,89 +1,33 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-import { Player, Mapa, Enemy } from './player.js';
+import { Mapa } from './player.js';
 import {GUI} from 'three/addons/libs/lil-gui.module.min.js';
 
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
 
-  const fov = 45;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 1000;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
+  const camera = new THREE.PerspectiveCamera(45, 2, 0.1, 1000);
+  camera.position.set(0,200,200);
 
+  
   const controls = new OrbitControls(camera, canvas);
   controls.target.set(0, 5, 0);
   controls.update();
+  
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color('black');
+  scene.background = new THREE.Color('gray');
 
   const mapa = new Mapa();
-  const enemy = new Enemy();
-  const player = new Player();
-  player.position.set(5 + 1, 5, 0);
+  scene.add(mapa.object3D);
 
-  scene.add(mapa);
-  scene.add(enemy);
-  scene.add(player);
+  const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light.position.set(0, 10, 0);
+  light.target.position.set(-5, 0, 0);
+  scene.add(light);
+  scene.add(light.target);
 
-
-  {
-    const sphereRadius = 3;
-    const sphereWidthDivisions = 32;
-    const sphereHeightDivisions = 16;
-    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
-    const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
-    const mesh = new THREE.Mesh(sphereGeo, sphereMat);
-    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
-    scene.add(mesh);
-  }
-
-  class ColorGUIHelper {
-    constructor(object, prop) {
-      this.object = object;
-      this.prop = prop;
-    }
-    get value() {
-      return `#${this.object[this.prop].getHexString()}`;
-    }
-    set value(hexString) {
-      this.object[this.prop].set(hexString);
-    }
-  }
-
-  function makeXYZGUI(gui, vector3, name, onChangeFn) {
-    const folder = gui.addFolder(name);
-    folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
-    folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
-    folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
-    folder.open();
-  }
-
-  {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.PointLight(color, intensity);
-    light.position.set(0, 10, 0);
-    scene.add(light);
-
-    const helper = new THREE.PointLightHelper(light);
-    scene.add(helper);
-
-    function updateLight() {
-      helper.update();
-    }
-
-    const gui = new GUI();
-    gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
-    gui.add(light, 'intensity', 0, 2, 0.01);
-    gui.add(light, 'distance', 0, 40).onChange(updateLight);
-
-    makeXYZGUI(gui, light.position, 'position');
-  }
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -95,21 +39,20 @@ function main() {
     }
     return needResize;
   }
-
   function render() {
-
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
-
+    mapa.newFrame()
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
-
   requestAnimationFrame(render);
 }
 
 main();
+console.clear();
+//console.log("Fazer a indentificação do mouse no plano, baixei alguns exemplos está na pasta cannon-es")
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 app.get('/', (req, res)=>{res.sendFile('index.html', { root: __dirname });});
 app.get('/script.js', (req, res)=>{res.sendFile('script.js', { root: __dirname });});
 app.get('/player.js', (req, res)=>{res.sendFile('player.js', { root: __dirname });});
+app.get('*', (req, res)=>{res.sendFile(req.url, { root: __dirname });});
 app.listen(8080, function()
 {
-	console.log("When Server Starts, Log This");
+	console.log("Hex game on 8080");
 });
